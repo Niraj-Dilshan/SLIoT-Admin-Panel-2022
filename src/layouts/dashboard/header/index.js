21162e7599a56c93857c47c787cc3359affb262e
INFINITY <nirajnimantha@gmail.com>
2023-03-07 23:09:39 +0530

    fix

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -7,7 +7,6 @@ import { bgBlur } from '../../../utils/cssStyles';
 // components
 import Iconify from '../../../components/iconify';
 //
-import Searchbar from './Searchbar';
 import AccountPopover from './AccountPopover';
 
 // ----------------------------------------------------------------------
@@ -55,7 +54,6 @@ export default function Header({ onOpenNav }) {
           <Iconify icon="eva:menu-2-fill" />
         </IconButton>
 
-        <Searchbar />
         <Box sx={{ flexGrow: 1 }} />
 
         <Stack
