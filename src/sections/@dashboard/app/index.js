eb6480cde33de38179b183a48a79558d61a1b321
INFINITY <nirajnimantha@gmail.com>
2023-03-10 11:55:20 +0530

    fix

--- src/sections/@dashboard/app/index.js
+++ src/sections/@dashboard/app/index.js
@@ -2,4 +2,4 @@ export { default as AppTasks } from './AppTasks';
 export { default as AppNewsUpdate } from './AppNewsUpdate';
 export { default as AppWebsiteVisits } from './AppWebsiteVisits';
 export { default as AppWidgetSummary } from './AppWidgetSummary';
-export { default as AppConversionRates } from './AppConversionRates';
+export { default as AppDistricts } from './AppDistricts';
